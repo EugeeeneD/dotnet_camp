9449a453f3160e660bcf49d13da664f7b66e1658
EugeneDorosh <evdorow@gmail.com>
2023-01-18 22:43:29 +0200

    CRUD v1

---
+++ HW14_DoroshE/HW14/HW14/CRUD.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    public class CRUD
+    {
+        public void Select(string connectionString, string tableName)
+        {
+            using (SqlConnection con = new(connectionString))
+            {
+                try
+                {
+                    SqlCommand cm = new($"SELECT * FROM {tableName}", con);
+
+                    con.Open();
+                    cm.ExecuteNonQuery();
+                    SqlDataReader dr = cm.ExecuteReader();
+
+                    List<string> columns = GetDataReaderColumnNames(dr);
+
+                    while (dr.Read())
+                    {
+                        foreach (string column in columns)
+                        {
+                            Console.Write($"{column}: {dr[$"{column}"]}\t");
+                        }
+                        Console.WriteLine();
+                    }
+                    dr.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Got an error, " + ex.Message);
+                }
+            }
+        }
+
+        public void Insert(string connectionString, string query)
+        {
+            using (SqlConnection con = new(connectionString))
+            {
+                try
+                {
+                    SqlCommand cm = new(query, con);
+
+                    con.Open();
+                    cm.ExecuteNonQuery();
+
+                    Console.WriteLine("Successfully inserted data.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Got an error, " + ex.Message);
+                }
+            }
+        }
+
+        public void Update(string connectionString, string query)
+        {
+            using (SqlConnection con = new(connectionString))
+            {
+                try
+                {
+                    SqlCommand cm = new(query, con);
+                    con.Open();
+                    cm.ExecuteNonQuery();
+
+                    Console.WriteLine("Successfully updated row.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Got an error, " + ex.Message);
+                }
+            }
+        }
+
+        public void Delete(string connectionString, string table, string column, string value)
+        {
+            using (SqlConnection con = new(connectionString))
+            {
+                try
+                {
+                    string query = $"DELETE FROM {table} WHERE {column} = '{value}';";
+                    SqlCommand cm = new(query, con);
+                    con.Open();
+                    cm.ExecuteNonQuery();
+
+                    Console.WriteLine("Row successfully deleted.");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Got an error, " + ex.Message);
+                }
+            }
+        }
+        List<string> GetDataReaderColumnNames(IDataReader rdr)
+        {
+            var columnNames = new List<string>();
+            for (int i = 0; i < rdr.FieldCount; i++)
+                columnNames.Add(rdr.GetName(i));
+            return columnNames;
+        }
+    }
+}
---
+++ HW14_DoroshE/HW14/HW14/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1;
+
+string connectionString = "Data Source=(local);Initial Catalog=SigmaCinemaDB;"
+            + "Integrated Security=true";
+
+CRUD test = new();
+
+//SELECT
+test.Select(connectionString, "Movies");
+
+//INSERT
+string queryForInserting = "INSERT INTO Movies VALUES (NEWID(), 'GTO', 'No Description');";
+test.Insert(connectionString, queryForInserting);
+
+//UPDATE
+string queryForUpdating = "UPDATE Movies SET Description = 'Some kind of description' WHERE Name = 'GTO';";
+test.Update(connectionString, queryForUpdating);
+
+//DELETE
+test.Delete(connectionString, "Movies", "Name", "GTO");
