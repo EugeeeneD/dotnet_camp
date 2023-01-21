8a46362674261aa38499866e8b963ac755b09b5c
EugeneDorosh <evdorow@gmail.com>
2023-01-21 20:59:48 +0200

    initial for repos

---
+++ HW15_DoroshE/HW15/HW15/Services/CinemaHallsService.cs
@@ -0,0 +1,37 @@
+﻿using HW15.Data;
+using HW15.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW15.Services
+{
+    public class CinemaHallsService : Context
+    {
+        public CinemaHallsService(CinemaDBContext context) : base(context)
+        {
+        }
+
+        public CinemaHallsService() : base()
+        {
+        }
+
+        public List<CinemaHalls> GetAllCinemaHalls()
+        {
+            return _context.CinemaHalls.ToList();
+        }
+
+        public CinemaHalls GetCinemaHallByAddress(string address)
+        {
+            return _context.CinemaHalls.FirstOrDefault(x => x.Address == address);
+        }
+
+        public void AddCinemaHalls(CinemaHalls cinemaHall)
+        {
+            await _context.CinemaHalls.AddAsync(cinemaHall);
+            _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ HW15_DoroshE/HW15/HW15/Services/Context.cs
@@ -0,0 +1,23 @@
+﻿using HW15.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW15.Services
+{
+    public class Context
+    {
+        protected CinemaDBContext _context;
+        public Context()
+        {
+            _context = new CinemaDBContext();
+        }
+
+        public Context(CinemaDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
