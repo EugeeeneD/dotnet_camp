65f01cc425a672030dabd2a32c35fa17557a67da
nonseverus <evdorow@gmail.com>
2023-01-26 18:28:45 +0200

    xUnit and Moq start

---
+++ HW15_DoroshE/HW15/Test/UserTests.cs
@@ -0,0 +1,24 @@
+﻿using HW15.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using HW15.Interface;
+
+namespace Test
+{
+    public class UserTests
+    {
+        public Mock<IUserService> userService = new();
+
+        [Fact]
+        public void AddUser(User user)
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
