616c1e06e02b143994476f8a7c5f98e5d2636c33
EugeneDorosh <evdorow@gmail.com>
2023-01-28 00:49:08 +0200

    variant with InMemory and Mock

--- HW15_DoroshE/HW15/Test/UserTests.cs
+++ HW15_DoroshE/HW15/Test/UserTests.cs
@@ -9,6 +9,7 @@ using HW15.Data;
 using HW15.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Reflection.Metadata;
 
 namespace Test
 {
@@ -23,9 +24,9 @@ namespace Test
         [InlineData("asdad'@gmail")]
         [InlineData("asdad\"@gmail")]
         [InlineData("asdad\\'@gmail")]
-        public void Add_InvalidEmails_ShouldThrowException(string email)
+        public void Add_InvalidEmails_ShouldThrowException_InMemoryVersion(string email)
         {
-            //Arrange
+            //Arrange 
             var options = new DbContextOptionsBuilder<CinemaDBContext>()
                 .UseInMemoryDatabase(databaseName: "CinemaNetwork")
                 .Options;
@@ -50,7 +51,7 @@ namespace Test
         [Theory]
         [InlineData("asdad@gmail.com")]
         [InlineData("asdad@ymail.com")]
-        public void Add_ValidEmails_ShouldAddUser(string email)
+        public void Add_ValidEmails_ShouldAddUser_InMemoryVersion(string email)
         {
             //Arrange
             var options = new DbContextOptionsBuilder<CinemaDBContext>()
@@ -60,9 +61,8 @@ namespace Test
             using (var context = new CinemaDBContext(options))
             {
                 UserService userService = new UserService(context);
-                var users = userService.FindAll().ToList();
 
-                int usersCountBefore = users.Count;
+                int usersCountBefore = context.Users.Count();
 
                 User user = new()
                 {
@@ -76,9 +76,70 @@ namespace Test
                 userService.Add(user);
 
                 //Assert
-                users = userService.FindAll().ToList();
-                Assert.Equal(usersCountBefore + 1, users.Count);
+                Assert.Equal(usersCountBefore + 1, context.Users.Count());
             }
         }
+
+/*        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("@")]
+        [InlineData("asdasd@")]
+        [InlineData("asdad[]@gmail")]
+        [InlineData("asdad'@gmail")]
+        [InlineData("asdad\"@gmail")]
+        [InlineData("asdad\\'@gmail")]
+        public void Add_InvalidEmails_ShouldThrowException_MockVersion(string email)
+        {
+            //Arrange 
+            var contextMock = new Mock<CinemaDBContext>();
+            UserService userService = new(contextMock.Object);
+
+            User user = new()
+            {
+                Email = email,
+                FirstName = "Test",
+                LastName = "Test",
+                Id = Guid.NewGuid()
+            };
+
+            //Assert And Act
+            Assert.Throws<ArgumentException>(() => userService.Add(user));
+        }
+
+        [Theory]
+        [InlineData("asdad@gmail.com")]
+        [InlineData("asdad@ymail.com")]
+        public void Add_ValidEmails_ShouldAddUser_MockVersion(string email)
+        {
+            //Arrange
+            var contextMock = new Mock<CinemaDBContext>();
+
+            var users = new List<User>().AsQueryable();
+
+            var mockSet = new Mock<DbSet<User>>();
+            mockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(users.Provider);
+            mockSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(users.Expression);
+            mockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(users.ElementType);
+            mockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(() => users.GetEnumerator());
+
+            contextMock.Setup(x => x.Users)
+
+            UserService userService = new(contextMock.Object);
+
+            User user = new()
+            {
+                Email = email,
+                FirstName = "Test",
+                LastName = "Test",
+                Id = Guid.NewGuid()
+            };
+
+            //Act
+            userService.Add(user);
+
+            //Assert
+            Assert.Equal(usersCountBefore + 1, contextMock.Object.Users.Count());
+        }*/
     }
 }
